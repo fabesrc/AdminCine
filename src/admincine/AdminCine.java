f642b40724fce037cc1f144baaaf50a580e31358
Cristian Navarro <95457181+SoyCristiann@users.noreply.github.com>
2025-05-09 00:40:22 -0500

    Se crean metodos en la clase AdminCine. Se hacen ajustes en las clases Pelicula y Funcion, necesarios para los métodos creados. Se ajustan algunos constructores de clases y se eliminan los super() al no ser necesarios en clases sin herencia.

--- src/admincine/AdminCine.java
+++ src/admincine/AdminCine.java
@@ -1,10 +1,55 @@
 package admincine;
 
+import java.util.ArrayList;
+
+
 public class AdminCine {
+	private ArrayList<Pelicula> peliculas;
+	private ArrayList<Funcion> funciones;
 	
-	public Funcion crearFuncion() {
+	public AdminCine() {
+		this.peliculas= new ArrayList<>(); // Garantizo la inicialización de las listas a través del constructor.
+		this.funciones= new ArrayList<>();
+	}
+	
 		
-		return null; // momentaneamente
+	//Método para agregar una película a la lista, el retorno boolean garantiza que el método se ejecutó de forma correcta pensando en pruebas.
+	public boolean crearPelicula(String nombre, String duracion, String director) {
+		peliculas.add(new Pelicula (nombre, duracion, director));
+		return true;
+	}
+	
+	// Muestra el listado de peliculas creadas.
+	public boolean verPeliculas() {
+		for(Pelicula p: peliculas) {
+			System.out.println(p);
+		}
+		return true;
+	}
+	
+	//Este método es necesario para buscar una película y agregarla al crear función.
+	public Pelicula buscarPelicula(String pelicula) {
+		for(Pelicula p: peliculas) {
+			if(p.getNombrePeli().equalsIgnoreCase(pelicula)) {
+				return p;
+			}
+		}
+		System.out.println("La película no se encuentra en el listado disponible");// 
+		return null;
+	}
+	
+	
+	//Para crear una funcion se depende de las clases sala y pelicula.
+	public boolean crearFuncion(String id, String horario, Pelicula pelicula, Sala sala) {
+		funciones.add(new Funcion(id, horario, pelicula, sala));		
+		return true;
+	}
+	
+	public boolean verFunciones() {
+		for(Funcion f: funciones) {
+			System.out.println(f);
+		}
+		return true;
 	}
 	
 	public Silla modificarAsiento() {
